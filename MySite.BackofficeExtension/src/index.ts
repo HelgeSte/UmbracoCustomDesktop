c5297d10ee9c8c88ab6acd046e0f4668e622c345
Helge Stegemoen <helge@stegemoen.com>
2025-10-07 16:32:24 +0200

    Added content to the src/index.ts file, and fixed import problem with "npm install -D @umbraco-cms/backoffice@14.3.4" and replaced imports with type imports.

---
+++ MySite.BackofficeExtension/src/index.ts
@@ -0,0 +1,12 @@
+/*import { UmbEntryPointOnInit } from '@umbraco-cms/backoffice/extension-api';
+import { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';*/
+import type { UmbEntryPointOnInit } from '@umbraco-cms/backoffice/extension-api';
+import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
+
+const manifests: Array<ManifestTypes> = [
+
+];
+
+export const onInit: UmbEntryPointOnInit = (_host, extensionRegistry) => {
+    extensionRegistry.registerMany(manifests);
+};
